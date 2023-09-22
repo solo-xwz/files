6a2a2f725ecd407571ba5cf18f71a78c0e8ff0c0
solo-xwz <89335154+solo-xwz@users.noreply.github.com>
2023-09-22 14:38:17 +0800

    Update update_openssl.sh

--- linux/update_openssl.sh
+++ linux/update_openssl.sh
@@ -1,40 +1,36 @@
 #!/bin/bash
 
-echo "当前 OpenSSL 版本: $(openssl version)"
-echo "当前 OpenSSL 路径: $(which openssl)"
 
-# 更新软件包列表并安装依赖项
-apt update
-apt install -y zlib1g zlib1g-dev gcc wget
+echo "current openssl version: $(openssl version)"
+echo "current openssl path: $(which openssl)"
+cd /usr/local &&
+echo "下载openssl"
 
-echo "安装 Perl..."
-wget https://www.cpan.org/src/5.0/perl-5.38.0.tar.gz
-tar -zxvf perl-5.38.0.tar.gz
-cd perl-5.38.0
-./Configure -des -Dprefix=/usr/local/perl -Dusethreads -Uversiononly
-make
-make install
+apt update &&
+apt install zlib1g zlib1g-dev gcc &&
 
-echo "安装 OpenSSL..."
-cd /usr/local
-wget https://www.openssl.org/source/openssl-1.1.1w.tar.gz
-tar -zxvf openssl-1.1.1w.tar.gz
-cd openssl-1.1.1w
+echo "安装perl..."
+wget https://www.cpan.org/src/5.0/perl-5.38.0.tar.gz &&
+tar -zxvf perl-5.38.0.tar.gz &&
+mkdir /usr/local/perl &&
+cd perl-5.38.0 &&
+./Configure -des -Dprefix=/usr/local/perl -Dusethreads –Uversiononly &&
+make &&
+make install &&
+
+echo "安装openssl..." &&
+wget https://www.openssl.org/source/openssl-3.0.11.tar.gz &&
+tar -zxvf openssl-3.0.11.tar.gz &&
+cd openssl-3.0.11 &&
 ./config shared zlib
-make
-make install
+ 
+make &&
+make install &&
 
-# 备份旧的 OpenSSL
-mv /usr/bin/openssl /usr/bin/openssl.old
-mv /usr/include/openssl /usr/include/openssl.old
-mv /usr/lib/openssl /usr/lib/openssl.old
-
-# 创建符号链接
+mv /usr/bin/openssl /usr/bin/openssl.old &&
 ln -s /usr/local/bin/openssl /usr/bin/openssl
-ln -s /usr/local/include/openssl /usr/include/openssl
+ln -s /usr/local/include/openssl/ /usr/include/openssl &&
+echo "/usr/local/lib/" >> /etc/ld.so.conf
+ldconfig -v &&
 
-# 更新库路径并加载动态链接器配置
-echo "/usr/local/lib" >> /etc/ld.so.conf
-ldconfig -v
-
-echo "新 OpenSSL 版本: $(openssl version)"
+echo "new openssl version: $(openssl version)"
