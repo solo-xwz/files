1d2a7279b0c0d2dd53457945142d8b1fa52106e7
solo-xwz <89335154+solo-xwz@users.noreply.github.com>
2023-09-22 14:54:36 +0800

    Update update_openssl.sh

--- linux/update_openssl.sh
+++ linux/update_openssl.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -e
 
 echo "current openssl version: $(openssl version)"
 echo "current openssl path: $(which openssl)"
@@ -7,16 +7,16 @@ cd /usr/local &&
 echo "下载openssl"
 
 apt update &&
-apt install zlib1g zlib1g-dev gcc &&
+apt install -y zlib1g zlib1g-dev gcc &&
 
 echo "安装perl..."
 wget https://www.cpan.org/src/5.0/perl-5.38.0.tar.gz &&
 tar -zxvf perl-5.38.0.tar.gz &&
 mkdir /usr/local/perl &&
 cd perl-5.38.0 &&
-./Configure -des -Dprefix=/usr/local/perl -Dusethreads –Uversiononly &&
-make &&
-make install &&
+./Configure -des -Dprefix=/usr/local/perl -Dusethreads -Uversiononly &&
+make && make install &&
+
 mv /usr/bin/perl /usr/bin/perl.old &&
 ln -s /usr/local/perl/bin/perl /usr/bin/perl &&
 
@@ -24,13 +24,11 @@ echo "安装openssl..." &&
 wget https://www.openssl.org/source/openssl-3.0.11.tar.gz &&
 tar -zxvf openssl-3.0.11.tar.gz &&
 cd openssl-3.0.11 &&
-./config shared zlib
- 
-make &&
-make install &&
+./config shared zlib &&
+make && make install &&
 
 mv /usr/bin/openssl /usr/bin/openssl.old &&
-ln -s /usr/local/bin/openssl /usr/bin/openssl
+ln -s /usr/local/bin/openssl /usr/bin/openssl &&
 ln -s /usr/local/include/openssl/ /usr/include/openssl &&
 echo "/usr/local/lib/" >> /etc/ld.so.conf
 ldconfig -v &&
