5176aaa9b4fcb9d6766392f0827f615f493c5d2a
solo-xwz <89335154+solo-xwz@users.noreply.github.com>
2023-09-22 19:23:40 +0800

    Update update_openssl.sh

--- linux/update_openssl.sh
+++ linux/update_openssl.sh
@@ -10,15 +10,17 @@ apt update &&
 apt install -y zlib1g zlib1g-dev gcc &&
 
 echo "安装perl..."
-wget https://www.cpan.org/src/5.0/perl-5.38.0.tar.gz &&
-tar -zxvf perl-5.38.0.tar.gz &&
-mkdir /usr/local/perl &&
-cd perl-5.38.0 &&
-./Configure -des -Dprefix=/usr/local/perl -Dusethreads -Uversiononly &&
-make && make install &&
+# wget https://www.cpan.org/src/5.0/perl-5.38.0.tar.gz &&
+# tar -zxvf perl-5.38.0.tar.gz &&
+# mkdir /usr/local/perl &&
+# cd perl-5.38.0 &&
+# ./Configure -des -Dprefix=/usr/local/perl -Dusethreads -Uversiononly &&
+# make && make install &&
 
-mv /usr/bin/perl /usr/bin/perl.old &&
-ln -s /usr/local/perl/bin/perl /usr/bin/perl &&
+# mv /usr/bin/perl /usr/bin/perl.old &&
+# ln -s /usr/local/perl/bin/perl /usr/bin/perl &&
+# curl -L https://install.perlbrew.pl | bash
+# alias perlbrew='~/perl5/perlbrew/bin/perlbrew' && perlbrew install perl-5.38.0
 
 echo "安装openssl..." &&
 cd /usr/local &&
