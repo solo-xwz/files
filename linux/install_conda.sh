8b99006f1c24e202f454534d7dc0f7f134e74343
solo-xwz <89335154+solo-xwz@users.noreply.github.com>
2023-09-22 14:57:05 +0800

    Update install_conda.sh

--- linux/install_conda.sh
+++ linux/install_conda.sh
@@ -3,12 +3,12 @@
 echo "start install miniconda3..."
 mkdir -p ~/miniconda3
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
-bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3 &&
 rm -rf ~/miniconda3/miniconda.sh
 echo "delete miniconda.sh..."
 echo "init miniconda..."
-~/miniconda3/bin/conda init bash
+~/miniconda3/bin/conda init bash &&
 echo "export ENV..."
-echo 'export PATH=/root/miniconda3/bin:$PATH' >> ~/.bashrc
-source ~/.bashrc
+echo 'export PATH=/root/miniconda3/bin:$PATH' >> ~/.bashrc &&
+source ~/.bashrc &&
 echo "install success!"
