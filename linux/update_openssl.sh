9d4f5c8b0e29ccd45306e96e3ef48a36726ebd5a
solo-xwz <89335154+solo-xwz@users.noreply.github.com>
2023-09-22 11:25:49 +0800

    add file

---
+++ linux/update_openssl.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+echo "current openssl version: $(openssl version)"
+echo "current openssl path: $(which openssl)"
+cd /usr/local
+echo "下载openssl"
+
+apt update
+apt install zlib1g zlib1g-dev gcc
+
+echo "安装perl..."
+wget https://www.cpan.org/src/5.0/perl-5.38.0.tar.gz
+tar -zxvf perl-5.38.0.tar.gz
+mkdir /usr/local/perl
+cd perl-5.38.0
+./Configure -des -Dprefix=/usr/local/perl -Dusethreads –Uversiononly
+make
+make install
+
+echo "安装openssl..."
+wget https://www.openssl.org/source/openssl-1.1.1w.tar.gz
+cd openssl-1.1.1w
+ ./config shared zlib
+ 
+make
+make install
+
+mv /usr/bin/openssl /usr/bin/openssl.old
+mv /usr/lib64/openssl /usr/lib64/openssl.old
+ln -s /usr/local/bin/openssl /usr/bin/openssl
+ln -s /usr/local/include/openssl/ /usr/include/openssl
+echo "/usr/local/lib/" >> /etc/ld.so.conf
+ldconfig -v
+
+echo "new openssl version: $(openssl version)"
