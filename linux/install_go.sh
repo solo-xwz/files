9e5daa9978d9cf2f63a32d12ef178d5fc136a132
solo-xwz <89335154+solo-xwz@users.noreply.github.com>
2023-09-23 10:53:39 +0800

    Update install_go.sh

--- linux/install_go.sh
+++ linux/install_go.sh
@@ -4,5 +4,17 @@ set -e
 cd /usr/local/ &&
 wget https://go.dev/dl/go1.21.1.linux-amd64.tar.gz &&
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.1.linux-amd64.tar.gz &&
-export PATH=$PATH:/usr/local/go/bin && export GO111MODULE=on
+
+# add env
+new_path='export PATH=$PATH:/usr/local/go/bin'
+
+# 检查~/.bashrc是否已经包含该行
+if grep -qF "$new_path" ~/.bashrc; then
+    echo "The PATH line already exists in ~/.bashrc. No changes made."
+else
+    # 如果~/.bashrc中不存在该行，则添加
+    echo "$new_path" >> ~/.bashrc
+    echo "PATH line added to ~/.bashrc."
+fi
+source ~/.bashrc && export GO111MODULE=on
 echo "$(go version)"
